648789c935e2c37dfd556bfaa5b77981d879b479
Bikram Sodhi <biksodhi@gmail.com>
2020-03-11 17:34:41 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,11 +8,9 @@ def my_all?(collection)
     i += 1
   end
 
-end
-
-if block_return_values.include? (false)
-  false
-else
+  if block_return_values.include? (false)
+    false
+  else
   true
-end
+
 end
